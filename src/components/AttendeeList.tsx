f39d05170b0bede16df330595642a71961f12b30
Denison Kolling <denisonkolling@gmail.com>
2024-04-24 09:06:34 -0300

    feat: add table components and data

--- src/components/AttendeeList.tsx
+++ src/components/AttendeeList.tsx
@@ -1,120 +1,122 @@
+import { ChangeEvent, useState } from 'react';
 import { ChevronLeft, ChevronRight, ChevronsLeft, ChevronsRight, MoreHorizontal, Search } from 'lucide-react';
+import { attendees } from '../data/attendees';
+import { Table } from './Table/Table';
+import { TableHeader } from './Table/TableHeader';
+import { TableRow } from './Table/TableRow';
+import { TableCell } from './Table/TableCell';
+import { IconButton } from './IconButton';
+import dayjs from 'dayjs';
+import 'dayjs/locale/pt-br';
+import relativeTime from 'dayjs/plugin/relativeTime';
+
+dayjs.extend(relativeTime);
+dayjs.locale('pt-br');
 
 const AttendeeList = () => {
+	const [search, setSearch] = useState('');
+	const [page, setPage] = useState(1);
+
+	const totalPages = Math.ceil(attendees.length / 10);
+
+	function onSearchInputChanged(event: ChangeEvent<HTMLInputElement>) {
+		setSearch(event.target.value);
+	}
+
+	function goToFirstPage() {
+		setPage(1);
+	}
+
+	function goToLastPage() {
+		setPage(totalPages);
+	}
+
+	function goToPreviousPage() {
+		setPage(page - 1);
+	}
+
+	function goToNextPage() {
+		setPage(page + 1);
+	}
+
 	return (
-		<>
+		<div className="flex flex-col gap-4">
 			<div className="flex gap-3 items-center">
 				<h1 className="text-2xl font-bold">Participantes</h1>
-				<div className="px-3 w-72 py-1.5 border rounded-lg border-white/25 bg-transparent text-sm flex items-center gap-3">
+				<div className="px-3 w-72 py-1.5 border border-white/10 rounded-lg flex items-center gap-3">
 					<Search className="size-4" />
-					<input className="bg-transparent flex-1 outline-none border-0 p-0 text-sm" placeholder="Buscar participante..." />
+					<input className="bg-transparent flex-1 outline-none border-0 p-0 text-sm" placeholder="Buscar participante..." onChange={onSearchInputChanged} />
 				</div>
+				{search}
 			</div>
 
-			<div className="border border-white/10 rounded-lg">
-				<table className="w-full">
-					<thead>
-						<tr className="border-b border-white/10">
-							<th style={{ width: 48 }} className="py-3 px-4 text-sm font-semibold text-left">
-								<input type="checkbox" className="size-4 bg-black/20 rounded border border-white/10" />
-							</th>
-							<th className="py-3 px-2.5 text-sm font-semibold text-left">Código</th>
-							<th className="py-3 px-2.5 text-sm font-semibold text-left">Participante</th>
-							<th className="py-3 px-2.5 text-sm font-semibold text-left">Data de inscrição</th>
-							<th className="py-3 px-2.5 text-sm font-semibold text-left">Data de check-in</th>
-							<th style={{ width: 64 }} className="py-3 px-4 text-sm font-semibold text-left"></th>
-						</tr>
-					</thead>
-
-					<tbody>
-						<tr className="border-b border-white/10 hover:bg-white/5">
-							<td className="py-3 px-4 text-sm text-zinc-300">
-								<input type="checkbox" className="size-4 bg-black/20 rounded border border-white/10" />
-							</td>
-							<td className="py-3 px-2.5 text-sm text-zinc-300">78546</td>
-							<td className="py-3 px-2.5 text-sm text-zinc-300">
-								<div className="flex flex-col">
-									<span>John Doe</span>
-									<span>johndoe@email.com</span>
-								</div>
-							</td>
-							<td className="py-3 px-2.5 text-sm text-zinc-300">16/04/2024</td>
-							<td className="py-3 px-2.5 text-sm text-zinc-300">22/04/2024</td>
-							<td className="py-3 px-4 text-sm text-zinc-300">
-								<button className="bg-black/20 border border-white/10 rounded-md p-1.5">
-									<MoreHorizontal className="size-4" />
-								</button>
-							</td>
-						</tr>
-						<tr className="border-b border-white/10 hover:bg-white/5">
-							<td className="py-3 px-4 text-sm text-zinc-300">
-								<input type="checkbox" className="size-4 bg-black/20 rounded border border-white/10" />
-							</td>
-							<td className="py-3 px-2.5 text-sm text-zinc-300">78546</td>
-							<td className="py-3 px-2.5 text-sm text-zinc-300">
-								<div className="flex flex-col">
-									<span>John Doe</span>
-									<span>johndoe@email.com</span>
-								</div>
-							</td>
-							<td className="py-3 px-2.5 text-sm text-zinc-300">16/04/2024</td>
-							<td className="py-3 px-2.5 text-sm text-zinc-300">22/04/2024</td>
-							<td className="py-3 px-4 text-sm text-zinc-300">
-								<button className="bg-black/20 border border-white/10 rounded-md p-1.5">
-									<MoreHorizontal className="size-4" />
-								</button>
-							</td>
-						</tr>
-						<tr className="border-b border-white/10 hover:bg-white/5">
-							<td className="py-3 px-4 text-sm text-zinc-300">
-								<input type="checkbox" className="size-4 bg-black/20 rounded border border-white/10" />
-							</td>
-							<td className="py-3 px-2.5 text-sm text-zinc-300">78546</td>
-							<td className="py-3 px-2.5 text-sm text-zinc-300">
-								<div className="flex flex-col">
-									<span>John Doe</span>
-									<span>johndoe@email.com</span>
-								</div>
-							</td>
-							<td className="py-3 px-2.5 text-sm text-zinc-300">16/04/2024</td>
-							<td className="py-3 px-2.5 text-sm text-zinc-300">22/04/2024</td>
-							<td className="py-3 px-4 text-sm text-zinc-300">
-								<button className="bg-black/20 border border-white/10 rounded-md p-1.5">
-									<MoreHorizontal className="size-4" />
-								</button>
-							</td>
-						</tr>
-					</tbody>
-					<tfoot>
-						<tr>
-							<td className="py-3 px-4 text-sm text-zinc-300" colSpan={3}>
-								Mostrando 10 de 228 itens
-							</td>
-							<td className="py-3 px-4 text-sm text-zinc-300 text-right" colSpan={3}>
-								<div className="inline-flex items-center gap-8">
-									<span>Página 1 de 23</span>
-
-									<div className="flex gap-1.5">
-										<button className="bg-white/10 border border-white/10 rounded-md p-1.5">
-											<ChevronsLeft className="size-4" />
-										</button>
-										<button className="bg-white/10 border border-white/10 rounded-md p-1.5">
-											<ChevronLeft className="size-4" />
-										</button>
-										<button className="bg-white/10 border border-white/10 rounded-md p-1.5">
-											<ChevronRight className="size-4" />
-										</button>
-										<button className="bg-white/10 border border-white/10 rounded-md p-1.5">
-											<ChevronsRight className="size-4" />
-										</button>
+			<Table>
+				<thead>
+					<tr className="border-b border-white/10">
+						<TableHeader style={{ width: 48 }}>
+							<input type="checkbox" className="size-4 bg-black/20 rounded border border-white/10" />
+						</TableHeader>
+						<TableHeader>Código</TableHeader>
+						<TableHeader>Participante</TableHeader>
+						<TableHeader>Data de inscrição</TableHeader>
+						<TableHeader>Data do check-in</TableHeader>
+						<TableHeader style={{ width: 64 }}></TableHeader>
+					</tr>
+				</thead>
+				<tbody>
+					{attendees.slice((page - 1) * 10, page * 10).map((ateendee) => {
+						return (
+							<TableRow key={ateendee.id}>
+								<TableCell>
+									<input type="checkbox" className="size-4 bg-black/20 rounded border border-white/10" />
+								</TableCell>
+								<TableCell>{ateendee.id}</TableCell>
+								<TableCell>
+									<div className="flex flex-col gap-1">
+										<span className="font-semibold text-white">{ateendee.name}</span>
+										<span>{ateendee.email}</span>
 									</div>
+								</TableCell>
+								<TableCell>{dayjs().to(ateendee.createdAt)}</TableCell>
+								<TableCell>{dayjs().to(ateendee.checkedInAt)}</TableCell>
+								<TableCell>
+									<IconButton transparent className="bg-black/20 border border-white/10 rounded-md p-1.5">
+										<MoreHorizontal className="size-4" />
+									</IconButton>
+								</TableCell>
+							</TableRow>
+						);
+					})}
+				</tbody>
+				<tfoot>
+					<tr>
+						<TableCell colSpan={3}>Mostrando 10 de {attendees.length} itens</TableCell>
+						<TableCell className="text-right" colSpan={3}>
+							<div className="inline-flex items-center gap-8">
+								<span>
+									Página {page} de {totalPages}
+								</span>
+
+								<div className="flex gap-1.5">
+									<IconButton onClick={goToFirstPage} disabled={page === 1}>
+										<ChevronsLeft className="size-4" />
+									</IconButton>
+									<IconButton onClick={goToPreviousPage} disabled={page === 1}>
+										<ChevronLeft className="size-4" />
+									</IconButton>
+									<IconButton onClick={goToNextPage} disabled={page === totalPages}>
+										<ChevronRight className="size-4" />
+									</IconButton>
+									<IconButton onClick={goToLastPage} disabled={page === totalPages}>
+										<ChevronsRight className="size-4" />
+									</IconButton>
 								</div>
-							</td>
-						</tr>
-					</tfoot>
-				</table>
-			</div>
-		</>
+							</div>
+						</TableCell>
+					</tr>
+				</tfoot>
+			</Table>
+		</div>
 	);
 };
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,14 @@
 import eventInIcon from '../assets/eventin-icon.svg';
+import { NavLink } from './NavLink';
 
 const Header = () => {
 	return (
 		<>
 			<div className="flex items-center gap-5 py-2">
 				<img src={eventInIcon} alt="" />
-				<nav>
-					<ul className="flex items-center gap-5">
-						<li>
-							<a href="" className="font-medium text-sm text-zinc-300">
-								Eventos
-							</a>
-						</li>
-						<li>
-							<a href="" className="font-medium text-sm">
-								Participantes
-							</a>
-						</li>
-					</ul>
+				<nav className="flex items-center gap-5">
+					<NavLink href="/eventos">Eventos</NavLink>
+					<NavLink href="/participantes">Participantes</NavLink>
 				</nav>
 			</div>
 		</>
